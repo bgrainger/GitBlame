be8dfd36b5b4a3b741508930da0823443ed78460
Bradley Grainger <bgrainger@gmail.com>
2011-03-28 18:46:19 -0700

    Add Invariant.Assert utility method.

---
+++ src/GitBlame/Utility/Invariant.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace GitBlame.Utility
+{
+	internal static class Invariant
+	{
+		/// <summary>
+		/// Verifies that <paramref name="condition"/> is <c>true</c>, and terminates the current process if not.
+		/// </summary>
+		/// <param name="condition">The condition to test.</param>
+		/// <param name="message">The error message if <paramref name="condition"/> is false.</param>
+		public static void Assert(bool condition, string message)
+		{
+			if (!condition)
+				Environment.FailFast(message);
+		}
+	}
+}
