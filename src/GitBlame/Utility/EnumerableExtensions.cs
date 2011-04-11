0c796eb644a9d82f1542f2b67a7ad7727700bfc7
Bradley Grainger <bgrainger@gmail.com>
2011-04-11 01:06:28 -0700

    Add ToDictionary extension method.

---
+++ src/GitBlame/Utility/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+namespace GitBlame.Utility
+{
+	/// <summary>
+	/// Helper methods for working with <c>IEnumerable</c>.
+	/// </summary>
+	public static class EnumerableExtensions
+	{
+		/// <summary>
+		/// Creates a dictionary from key value pairs.
+		/// </summary>
+		/// <typeparam name="TKey">The type of the key.</typeparam>
+		/// <typeparam name="TValue">The type of the value.</typeparam>
+		/// <param name="pairs">The key value pairs.</param>
+		/// <returns>The dictionary.</returns>
+		public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> pairs)
+		{
+			var dict = new Dictionary<TKey, TValue>();
+			foreach (var pair in pairs)
+				dict.Add(pair.Key, pair.Value);
+			return dict;
+		}
+	}
+}
