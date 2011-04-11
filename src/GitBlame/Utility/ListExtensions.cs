3bc0a642fe570644a0a513ba9fe83fb5cf8cd464
Bradley Grainger <bgrainger@gmail.com>
2011-04-11 01:06:36 -0700

    Add AsReadOnly extension method for IList<T>.

---
+++ src/GitBlame/Utility/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GitBlame.Utility
+{
+	/// <summary>
+	/// Methods for manipulating lists.
+	/// </summary>
+	public static class ListExtensions
+	{
+		/// <summary>
+		/// Wraps a <see cref="ReadOnlyCollection{T}"/> around the specified list.
+		/// </summary>
+		/// <typeparam name="T">The type of item in the list.</typeparam>
+		/// <param name="list">The list.</param>
+		/// <returns>A <see cref="ReadOnlyCollection{T}"/> that wraps the list.</returns>
+		public static ReadOnlyCollection<T> AsReadOnly<T>(this IList<T> list)
+		{
+			return new ReadOnlyCollection<T>(list);
+		}
+	}
+}
