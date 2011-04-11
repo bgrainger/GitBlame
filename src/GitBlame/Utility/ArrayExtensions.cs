c5a3609224c55f521563dc3c6e0c0dfa92569ed8
Bradley Grainger <bgrainger@gmail.com>
2011-04-11 01:06:41 -0700

    Add Clone extension method for arrays.

---
+++ src/GitBlame/Utility/ArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+namespace GitBlame.Utility
+{
+	/// <summary>
+	/// Helper methods for working with arrays.
+	/// </summary>
+	public static class ArrayExtensions
+	{
+		/// <summary>
+		/// Clones the specified array.
+		/// </summary>
+		/// <param name="array">The array to clone.</param>
+		/// <returns>A clone of the specified array.</returns>
+		/// <remarks>This method is merely useful in avoiding the cast that is otherwise necessary
+		/// when calling <see cref="System.Array.Clone" />.</remarks>
+		public static T[] Clone<T>(T[] array)
+		{
+			return (T[]) array.Clone();
+		}
+	}
+}
--- src/GitBlame/Utility/EnumerableExtensions.cs
+++ src/GitBlame/Utility/EnumerableExtensions.cs
@@ -1,5 +1,9 @@
 ﻿
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace GitBlame.Utility
 {
@@ -8,6 +12,53 @@ namespace GitBlame.Utility
 	/// </summary>
 	public static class EnumerableExtensions
 	{
+		/// <summary>
+		/// Groups the elements of a sequence according to a specified key selector function.  Each group consists of <b>consecutive</b> items having the same key. Order is preserved.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <typeparam name="TKey">The type of the key returned by keySelector.</typeparam>
+		/// <param name="source">A sequence whose elements to group.</param>
+		/// <param name="keySelector">A function to extract the key for each element.</param>
+		/// <returns>A sequence of IGroupings containing a sequence of objects and a key.</returns>
+		public static IEnumerable<IGrouping<TKey, TSource>> GroupConsecutiveBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+		{
+			if (source == null)
+				throw new ArgumentNullException("source");
+			if (keySelector == null)
+				throw new ArgumentNullException("keySelector");
+
+			return GroupConsecutiveImpl(source, keySelector, EqualityComparer<TKey>.Default);
+		}
+
+		private static IEnumerable<IGrouping<TKey, TSource>> GroupConsecutiveImpl<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+		{
+			using (IEnumerator<TSource> e = source.GetEnumerator())
+			{
+				if (!e.MoveNext())
+					yield break;
+
+				TKey lastKey = keySelector(e.Current);
+				List<TSource> values = new List<TSource> { e.Current };
+
+				while (e.MoveNext())
+				{
+					TKey currentKey = keySelector(e.Current);
+					if (comparer.Equals(lastKey, currentKey))
+					{
+						values.Add(e.Current);
+					}
+					else
+					{
+						yield return new Grouping<TKey, TSource>(lastKey, values.AsReadOnly());
+						lastKey = currentKey;
+						values = new List<TSource> { e.Current };
+					}
+				}
+
+				yield return new Grouping<TKey, TSource>(lastKey, values.AsReadOnly());
+			}
+		}
+
 		/// <summary>
 		/// Creates a dictionary from key value pairs.
 		/// </summary>
@@ -21,6 +72,68 @@ namespace GitBlame.Utility
 			foreach (var pair in pairs)
 				dict.Add(pair.Key, pair.Value);
 			return dict;
+		}
+
+		private class Grouping<TKey, TSource> : IGrouping<TKey, TSource>, ICollection<TSource>
+		{
+			public Grouping(TKey key, ReadOnlyCollection<TSource> list)
+			{
+				m_key = key;
+				m_list = list;
+			}
+
+			public IEnumerator<TSource> GetEnumerator()
+			{
+				return m_list.GetEnumerator();
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+			{
+				return m_list.GetEnumerator();
+			}
+
+			public TKey Key
+			{
+				get { return m_key; }
+			}
+
+			void ICollection<TSource>.Add(TSource item)
+			{
+				throw new NotSupportedException("Collection is read-only.");
+			}
+
+			void ICollection<TSource>.Clear()
+			{
+				throw new NotSupportedException("Collection is read-only.");
+			}
+
+			bool ICollection<TSource>.Contains(TSource item)
+			{
+				return m_list.Contains(item);
+			}
+
+			void ICollection<TSource>.CopyTo(TSource[] array, int arrayIndex)
+			{
+				m_list.CopyTo(array, arrayIndex);
+			}
+
+			bool ICollection<TSource>.Remove(TSource item)
+			{
+				throw new NotSupportedException("Collection is read-only.");
+			}
+
+			public int Count
+			{
+				get { return m_list.Count; }
+			}
+
+			bool ICollection<TSource>.IsReadOnly
+			{
+				get { return true; }
+			}
+
+			readonly TKey m_key;
+			readonly ReadOnlyCollection<TSource> m_list;
 		}
 	}
 }
