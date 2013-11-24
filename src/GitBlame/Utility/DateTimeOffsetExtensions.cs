90840eb13e3f80eace92e0b46b417437c416bf6a
Bradley Grainger <bgrainger@gmail.com>
2013-11-23 19:06:56 -0800

    Add DateTimeOffsetExtensions.

---
+++ src/GitBlame/Utility/DateTimeOffsetExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace GitBlame.Utility
+{
+	/// <summary>
+	/// Provides methods for manipulating dates.
+	/// </summary>
+	public static class DateTimeOffsetExtensions
+	{
+		/// <summary>
+		/// Converts the specified ISO 8601 representation of a date and time to its DateTimeOffset equivalent.
+		/// </summary>
+		/// <param name="value">The ISO 8601 string representation to parse.</param>
+		/// <returns>The DateTimeOffset equivalent.</returns>
+		public static DateTimeOffset ParseIso8601(string value)
+		{
+			return DateTimeOffset.ParseExact(value, Iso8601Format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+		}
+
+		/// <summary>
+		/// Converts the specified ISO 8601 representation of a date and time to its DateTimeOffset equivalent.
+		/// </summary>
+		/// <param name="value">The ISO 8601 string representation to parse.</param>
+		/// <param name="result">The DateTimeOffset equivalent.</param>
+		/// <returns>True if successful.</returns>
+		public static bool TryParseIso8601(string value, out DateTimeOffset result)
+		{
+			return DateTimeOffset.TryParseExact(value, Iso8601Format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out result);
+		}
+
+		/// <summary>
+		/// Formats the date in the standard ISO 8601 format.
+		/// </summary>
+		/// <param name="value">The date to format.</param>
+		/// <returns>The formatted date.</returns>
+		public static string ToIso8601(this DateTimeOffset value)
+		{
+			return value.ToString(Iso8601Format, CultureInfo.InvariantCulture);
+		}
+
+		/// <summary>
+		/// The ISO 8601 format string.
+		/// </summary>
+		public const string Iso8601Format = "yyyy'-'MM'-'dd'T'HH':'mm':'sszzz";
+	}
+}
